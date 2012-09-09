b11af69741e2d91f3b9a2ad07dce5dd8e2a51103
John A. Crow <jac@ncgr.org>
2012-09-08 22:32:47 -0600

    Flipped the sense of mkdist.sh.
    By default creates a prerelease.

--- mkdist.sh
+++ mkdist.sh
@@ -1,6 +1,8 @@
+# run ./mkdist.sh RELEASE for a formal release.
+
 if test z$1 = z; then
-  LDFLAGS=-L/usr/local/lib ./autogen.sh && LDFLAGS=-L/usr/local/lib make distcheck
-else
   PREDIST=1 LDFLAGS=-L/usr/local/lib ./autogen.sh && LDFLAGS=-L/usr/local/lib make distcheck
+else
+  LDFLAGS=-L/usr/local/lib ./autogen.sh && LDFLAGS=-L/usr/local/lib make distcheck
 fi
 
