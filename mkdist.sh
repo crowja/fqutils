4c35d43db8de5e9ddb52b195af71d436438889bc
John A. Crow <jac@ncgr.org>
2012-09-10 12:16:50 -0600

    Minor text fixes.

--- mkdist.sh
+++ mkdist.sh
@@ -3,8 +3,8 @@
 # run ./mkdist.sh RELEASE for a formal release.
 
 if test z$1 = z; then
-  PREDIST=1 LDFLAGS=-L/usr/local/lib ./autogen.sh && LDFLAGS=-L/usr/local/lib make distcheck
+  PREDIST=1 ./autogen.sh && make distcheck
 else
-  LDFLAGS=-L/usr/local/lib ./autogen.sh && LDFLAGS=-L/usr/local/lib make distcheck
+  ./autogen.sh && make distcheck
 fi
 
