b79e910bcd0db652c82dd185620de8dc4b85a012
John A. Crow <jac@ncgr.org>
2012-09-10 22:16:36 -0600

    Various.
    Introduce m4 and a few macros.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #! /bin/sh -e
 
-#export LDFLAGS='-L/usr/local/lib'
-
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 autoreconf --force --install --verbose "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+test -n "$NOCONFIGURE" || "${srcdir}/configure" "$@"
