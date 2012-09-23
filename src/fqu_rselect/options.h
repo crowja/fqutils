cecb0cc231fa42c5332c45f6f5201998c112c59a
John A. Crow <jac@ncgr.org>
2012-09-22 23:33:47 -0600

    Initial check-in of fqu_rselect.

---
+++ src/fqu_rselect/options.h
@@ -0,0 +1,11 @@
+struct options {
+   char       *fname;                       /* input file name, if any */
+   double      frac;
+   int         optind;
+   unsigned    verbosity;
+};
+
+struct options *options_new( void );
+void        options_free( struct options *p );
+void        options_helpmsg( FILE *out );
+void        options_cmdline( struct options *p, int argc, char *argv[] );
